ad3a80a753910913ef833eae4d054a328278669b
Kevin <kevin.yi93@gmail.com>
2020-11-10 14:25:31 -0500

    Center sign up form to center of page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -65,7 +65,7 @@ const Signup = () => {
     }
   }
   return (
-    <div>
+    <div className="sign-up-form">
       <form className={classes.form} noValidate autoComplete="off">
         <TextField
           required
