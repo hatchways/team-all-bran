86062e22b40870b6b194bf955d2ef37a31c203c8
unknown <nafis1030@gmail.com>
2020-11-12 15:06:57 -0500

    Updated routing to follow MVC model

---
+++ server/controllers/userController.js
@@ -0,0 +1,61 @@
+const userModel = require("../models/User");
+const validateRegister = require("../user-validation/register");
+const validateLogin = require("../user-validation/login");
+const { secretKey } = process.env;
+const jwt = require("jsonwebtoken");
+
+function register(req, res) {
+  const { errors, isValid } = validateRegister(req.body);
+
+  //validate registration
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  let user = userModel.registerUser(req);
+  user.then((data) => {
+    if (data.error) {
+      res.status(400).json({ error: data.error });
+    } else {
+      createTokenResponse(data.user, res);
+    }
+  });
+}
+
+function login(req, res) {
+  const { errors, isValid } = validateLogin(req.body);
+
+  //validate registration
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  let user = userModel.loginUser(req);
+  user.then((data) => {
+    if (data.error) {
+      res.status(400).json({ error: data.error });
+    } else {
+      createTokenResponse(data.user, res);
+    }
+  });
+}
+
+function createTokenResponse(user, res) {
+  const payload = { user };
+  return jwt.sign(
+    payload,
+    secretKey,
+    {
+      expiresIn: 2629744, // 1 month in seconds
+    },
+    (err, token) => {
+      let responseObj = { user: user, token: token };
+      res
+        .status(201)
+        .cookie("token", token, { httpOnly: true })
+        .json(responseObj);
+    }
+  );
+}
+
+module.exports = { register, login };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,118 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const userController = require("../controllers/userController"); // import controller
 
-// Input validation
-const validateRegister = require("../user-validation/register");
-const validateLogin = require("../user-validation/login");
-
-const User = require("../models/User");
+const { User } = require("../models/User");
 
 // POST /users/register
-router.post("/register", (req, res) => {
-  const { errors, isValid } = validateRegister(req.body);
-
-  //validate registration
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  // Check if email is in db
-  User.findOne({ email: req.body.email }).then((user) => {
-    if (user) {
-      return res.status(400).json({ email: "Email already exists" });
-    } else {
-      const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-      });
-
-      // Encrypt password before saving in db
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-
-          newUser
-            .save()
-            .then((user) => {
-              const payload = { user };
-
-              // Sign token
-              // Save key to an env file later
-              jwt.sign(
-                payload,
-                "secret",
-                {
-                  expiresIn: 2629744, // 1 month in seconds
-                },
-                (err, token) => {
-                  let responseObj = { user: user, token: `Bearer ${token}` };
-                  res
-                    .status(201)
-                    .cookie("token", token, { httpOnly: true })
-                    .json(responseObj);
-                }
-              );
-            })
-            .catch((err) => console.log(err));
-        });
-      });
-    }
-  });
-});
+router.post("/register", userController.register);
 
 // POST /users/login
-router.post("/login", (req, res) => {
-  const { errors, isValid } = validateLogin(req.body);
-
-  // Validate Login
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const email = req.body.email;
-  const password = req.body.password;
-
-  // Find user by email
-  User.findOne({ email }).then((user) => {
-    // Check if user exists
-    if (!user) {
-      return res
-        .status(404)
-        .json({ error: "Username and password was incorrect" });
-    }
-
-    // Check password
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = { user };
-
-        // Sign token
-        // Save key to an env file later
-        jwt.sign(
-          payload,
-          "secret",
-          {
-            expiresIn: 2629744, // 1 month in seconds
-          },
-          (err, token) => {
-            let responseObj = { user: user, token: `Bearer ${token}` };
-            res.cookie("token", token, { httpOnly: true }).json(responseObj);
-          }
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ passwordincorrect: "Password incorrect" });
-      }
-    });
-  });
-});
+router.post("/login", userController.login);
 
 module.exports = router;
