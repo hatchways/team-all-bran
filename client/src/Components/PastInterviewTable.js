0b36889ee76ccebcbac2bdc556fe5d667972b983
Kevin <kevin.yi93@gmail.com>
2020-11-12 17:54:40 -0500

    Display stars for coding and communication ratings

--- client/src/Components/PastInterviewTable.js
+++ client/src/Components/PastInterviewTable.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import { useStyles } from '../themes/theme'
-
-function createPastInterviewData(name, codingRating, communcationRating, questions, detailedFeedback) {
-  return { name, codingRating, communcationRating, questions, detailedFeedback };
-}
-
-const pastInterviews = [
-  createPastInterviewData({ date: "Thursday, Apr 30, 2020", time: "10:00 AM - 11:00 AM" }, 4.5, 4.0, "View", "View"),
-  createPastInterviewData({ date: "Wednesday, Mar 18, 2020", time: "2:00 PM - 3:30 PM" }, 3.0, 4.5, "View", "View"),
-];
+import Rating from '@material-ui/lab/Rating';
 
 const PastInterviewTable = () => {
+
+  const createPastInterviewData = (name, codingRating, communcationRating, questions, detailedFeedback) => {
+    return { name, codingRating, communcationRating, questions, detailedFeedback };
+  }
+
+  const pastInterviews = [
+    createPastInterviewData({ date: "Thursday, Apr 30, 2020", time: "10:00 AM - 11:00 AM" }, 4.5, 4.0, "View", "View"),
+    createPastInterviewData({ date: "Wednesday, Mar 18, 2020", time: "2:00 PM - 3:30 PM" }, 3.0, 4.5, "View", "View"),
+  ];
+
   const classes = useStyles();
 
   return (
@@ -44,8 +44,12 @@ const PastInterviewTable = () => {
                   {interview.name.time}
                 </div>
               </TableCell>
-              <TableCell align="right">{interview.codingRating}</TableCell>
-              <TableCell align="right">{interview.communcationRating}</TableCell>
+              <TableCell align="right">
+                <Rating name="read-only" value={interview.codingRating} readOnly />
+              </TableCell>
+              <TableCell align="right">
+                <Rating name="read-only" value={interview.communcationRating} readOnly />
+              </TableCell>
               <TableCell align="right">{interview.questions}</TableCell>
               <TableCell align="right">{interview.detailedFeedback}</TableCell>
             </TableRow>
