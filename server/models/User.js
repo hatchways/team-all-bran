6183de9c915842ebd9d5c666869e3de2eb0a610e
Rines.io <tsrines@gmail.com>
2020-11-12 19:30:15 -0500

    toObject transform, dealt with password being sent with user object

--- server/models/User.js
+++ server/models/User.js
@@ -1,69 +1,85 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 // Create Schema
-const UserSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toObject: {
+      transform: function (doc, ret) {
+        delete ret.password;
+      },
+    },
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.password;
+      },
+    },
+  }
+);
 
-const User = mongoose.model("users", UserSchema);
+const User = mongoose.model('users', UserSchema);
 
-function registerUser(req) {
+async function registerUser(req) {
   // Check if email is in db
-  return User.findOne({ email: req.body.email }).then((user) => {
-    if (user) {
-      return { error: "Email already exists" };
-    } else {
-      const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-      });
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return { error: 'Email already exists' };
+  }
 
-      newUser.password = bcrypt.hashSync(newUser.password, 10);
-
-      newUser.save();
-      return { user: newUser };
-    }
-  });
+  try {
+    let newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    newUser.password = bcrypt.hashSync(newUser.password, 10);
+    newUser = await newUser.save();
+    console.log(newUser);
+    return { user: newUser };
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function loginUser(req) {
+async function loginUser(req) {
   const email = req.body.email;
   const password = req.body.password;
 
   // Find user by email
-  return User.findOne({ email }).then((user) => {
-    // Check if user exists
-    if (!user) {
-      return { error: "Username and password was incorrect" };
-    }
+  const user = await User.findOne({ email });
+  // Check if user exists
+  if (!user) {
+    return { error: 'Username and password was incorrect' };
+  }
 
-    let validPass = bcrypt.compareSync(password, user.password);
+  let validPass = await bcrypt.compare(password, user.password);
 
-    if (validPass) {
-      return { user: user };
-    } else {
-      return { error: "Username and password was incorrect" };
-    }
-  });
+  if (validPass) {
+    return { user };
+  } else {
+    return { error: 'Username and password was incorrect' };
+  }
 }
 
 module.exports = { User, registerUser, loginUser };
