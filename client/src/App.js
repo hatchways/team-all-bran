0aeefcad6beca110f9ab941239be602709f0c60a
unknown <nafis1030@gmail.com>
2020-11-18 01:19:03 -0500

    Created simple compiler

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { MuiThemeProvider } from '@material-ui/core'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css'
-import { StateProvider, store } from './context/store'
-import { theme } from './themes/theme'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import { StateProvider, store } from './context/store';
+import { theme } from './themes/theme';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
-import Routes from './routing/Routes'
+import Routes from './routing/Routes';
 
 function App() {
   return (
@@ -17,13 +17,12 @@ function App() {
           <Switch>
             <Route exact path='/login' component={Login} />
             <Route exact path='/signup' component={Signup} />
-            <Route exact path='/test' component={UserInformation} />
             <Route component={Routes} />
           </Switch>
         </Router>
       </MuiThemeProvider>
     </StateProvider>
-  )
+  );
 }
 
-export default App
+export default App;
