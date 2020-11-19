d4fa62286e46f7b2fc8e635cc6b215e9d81ba73b
Rines.io <tsrines@gmail.com>
2020-11-19 14:16:26 -0500

    removed unused code

--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -1,17 +1,15 @@
 import React, { useContext } from 'react';
 import InterviewDialog from '../components/InterviewDialog';
 import { useStyles } from '../themes/theme';
-import { StartDashboardButton } from '../components/Buttons';
 import PastInterviewTable from '../components/PastInterviewTable';
 import { store } from '../context/store';
-import { Redirect, useHistory } from 'react-router';
+import { Redirect } from 'react-router';
 
 import UserInformation from '../components/UserInformation';
 
 const DashBoard = () => {
   const classes = useStyles();
 
-  const history = useHistory();
   const { state } = useContext(store);
 
   if (!state.isAuthenticated) return <Redirect to='/signup' />;
