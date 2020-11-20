d67bea628d6e209d95875479fdd45c2e06739371
Kevin <kevin.yi93@gmail.com>
2020-11-19 23:45:29 -0500

    FE: LoginSignupWrapper redirects auth. users to /dashboard

--- client/src/components/LoginSignupWrapper.js
+++ client/src/components/LoginSignupWrapper.js
@@ -4,38 +4,24 @@ import Grid from '@material-ui/core/Grid';
 import { useStyles } from '../themes/theme';
 import { useHistory } from 'react-router-dom';
 import { store } from '../context/store';
-import { USER_LOADED } from '../context/types';
 import axios from 'axios';
 
 const LoginSignupWrapper = ({ children }) => {
-  const history = useHistory();
   const classes = useStyles();
-  const { dispatch, state } = useContext(store);
+  const history = useHistory();
 
-  const redirectToDashBoard = useCallback(async () => {
+  const getUserInfo = useCallback(async () => {
     try {
-      let result = await axios.get('http://localhost:3001/users/', {
-        params: {
-          token: state.token,
-        },
-      });
-
-      dispatch({
-        type: USER_LOADED,
-        payload: result.data.user,
-      });
-
-      history.push('/dashboard');
+      await axios.get('http://localhost:3000/users/')
+      history.push('/dashboard')
     } catch (error) {
-      console.log(error);
     }
-  }, [dispatch, history, state.token]);
+  }, []);
 
   useEffect(() => {
-    if (state.token) {
-      redirectToDashBoard();
-    }
-  }, [redirectToDashBoard, state.token]);
+    getUserInfo();
+  }, [getUserInfo]);
+
 
   return (
     <div className={classes.loginSignupWrapperRoot}>
