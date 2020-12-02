dc0385a0c927c13214364b74d00db9f5ccbea28c
Kevin <kevin.yi93@gmail.com>
2020-12-01 19:32:04 -0500

    Display video inside TextEditor component

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,9 +1,19 @@
 import React, { useRef, useState } from 'react';
 import { ControlledEditor } from '@monaco-editor/react';
 
-const TextEditor = ({ value, language, handleCodeSnippetChange }) => {
+const TextEditor = (
+  {
+    handleEndCall,
+    handleAcceptCall,
+    receivingCall,
+    value,
+    language,
+    handleCodeSnippetChange,
+    userVideo,
+    partnerVideo,
+    callAccepted
+  }) => {
   const [isEditorReady, setIsEditorReady] = useState(false);
-  console.log(isEditorReady);
 
   const editorRef = useRef();
 
@@ -15,18 +25,35 @@ const TextEditor = ({ value, language, handleCodeSnippetChange }) => {
   const handleCodeChange = (_, value) => {
     handleCodeSnippetChange(value);
   };
-
   return (
-    <ControlledEditor
-      height='90vh'
-      theme='dark'
-      options={{ fontSize: 18 }}
-      language={language}
-      value={value}
-      editorDidMount={handleEditorDidMount}
-      onChange={handleCodeChange}
-      loading={'Loading...'}
-    />
+    <div>
+      <div>
+        <video playsInline muted ref={userVideo} autoPlay />
+        <video playsInline muted ref={partnerVideo} autoPlay />
+        {receivingCall ?
+          <div>
+            <h1>Tim Rines is calling you</h1>
+            <button onClick={handleAcceptCall}>Accept</button>
+          </div>
+          :
+          <div>
+            <button onClick={handleEndCall}>End</button>
+          </div>
+        }
+      </div>
+      <div>
+        <ControlledEditor
+          height='90vh'
+          theme='dark'
+          options={{ fontSize: 18 }}
+          language={language}
+          value={value}
+          editorDidMount={handleEditorDidMount}
+          onChange={handleCodeChange}
+          loading={'Loading...'}
+        />
+      </div>
+    </div>
   );
 };
 
